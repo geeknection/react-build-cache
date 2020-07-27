d658c5d5409308d62503af60ac077ac11eb8684b
Bruno Nascimento <br.dev.jobs@gmail.com>
2020-07-27 18:03:08 -0300

    version 1.0.2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # react-build-cache
 Clean reactjs cache in build version
+## Getting started
+
+`npm install react-build-cache --save-dev`
+
+## Usage
+
+In your package file found scripts/build and rewrite to: react-scripts build && node_modules/react-build-cache
---
+++ index.js
@@ -0,0 +1,14 @@
+String.prototype.allReplace = function(obj) {
+    var retStr = this;
+    for (var x in obj) {
+        retStr = retStr.replace(new RegExp(x, 'g'), obj[x]);
+    }
+    return retStr;
+};
+const fs = require('fs');
+let contents = fs.readFileSync('./build/index.html').toString();
+contents = contents.allReplace({'.js"': `.js?v=${new Date().getTime()}"`, '.css"': `.css?v=${new Date().getTime()}"`});
+fs.writeFile('./build/index.html', contents, function (err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+});
