6a5fbeeb42caa67f8a6de430af6f32ce638fb3a5
Bruno Nascimento <robertbrunotrabalho@gmail.com>
2021-03-20 19:23:47 -0300

    Fs package

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ String.prototype.allReplace = function(obj) {
     }
     return retStr;
 };
-const fs = require('fs');
+const fs = require('fs-extra');
 let contents = fs.readFileSync(`${process.cwd()}/build/index.html`).toString();
 contents = contents.allReplace({'.js"': `.js?v=${new Date().getTime()}"`, '.css"': `.css?v=${new Date().getTime()}"`});
 fs.writeFile(`${process.cwd()}/build/index.html`, contents, function (err) {
