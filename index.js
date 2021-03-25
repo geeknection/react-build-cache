3409d8f1087fc1f2f7d2830cb5655e1dbe1f7441
Bruno Nascimento <robertbrunotrabalho@gmail.com>
2021-03-24 21:49:57 -0300

    Geral

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ String.prototype.allReplace = function(obj) {
 };
 const fs = require('fs-extra');
 let contents = fs.readFileSync(`${process.cwd()}/build/index.html`).toString();
-contents = contents.allReplace({'.js"': `.js?v=${new Date().getTime()}"`, '.css"': `.css?v=${new Date().getTime()}"`});
+contents = contents.allReplace({
+    '.js"': `.js?v=${new Date().getTime()}"`,
+    '.css"': `.css?v=${new Date().getTime()}"`
+});
 fs.writeFile(`${process.cwd()}/build/index.html`, contents, function (err) {
     if (err) throw err;
     console.log('It\'s saved!');
